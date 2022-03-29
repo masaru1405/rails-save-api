7cb53b1481760c4a44e6d92e33166a04cf78e8d7
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-03-29 11:50:22 -0300

    add search bar

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,6 +1,15 @@
 class PersonsController < ApplicationController
   def index
-    #@persons = Person.all
-    @persons = Person.page params[:page]
+    if !params[:search].nil? #if search is not nil
+      @persons = Person.search(params[:search]).page(params[:page])
+    else
+      @persons = Person.page params[:page]
+    end
+    #@persons = Person.search(params[:search])
   end
+
+  private
+    def persons_params
+      params.require(:person).permit(:search)
+    end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,12 @@
 class Person < ApplicationRecord
+   
    paginates_per 10
+
+   def self.search(search)
+      person = Person.where(first_name: search)
+      if person.count == 0 #sem resultado
+          return Person.all
+      end
+      return person
+   end
 end
